97967fbd5484156655df1ab99114f8df648a2ed8
DESKTOP-NTP1PFC\fabie <fabiencapoen1997@gmail.com>
2023-05-06 18:23:16 +0200

    [++][UPD][Finalisation envoi formulaire copie] finalisation envoi formulaire copie OK

--- src/Controller/CopieCorrectionController.php
+++ src/Controller/CopieCorrectionController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 use App\Entity\Copie;
 use App\Entity\Quiz;
+use App\Entity\Reponse;
 use App\Trait\CopieCorrection;
 use App\Trait\QuizSuiviTrait;
+use Doctrine\ORM\EntityManagerInterface;
 use http\Client\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -48,11 +50,38 @@ class CopieCorrectionController extends AbstractController
     }
 
     #[Route('/copie/correction/flush/{id}', name: 'app_copie_correction_flush')]
-    public function CopieCorrectionFlush(Copie $copie, \Symfony\Component\HttpFoundation\Request $request): Response
+    public function CopieCorrectionFlush(Copie $copie,EntityManagerInterface $manager, \Symfony\Component\HttpFoundation\Request $request): Response
     {
-        $currentUser = $this->getUser();
-        $request->getContent();
-        dd($request);
+        $reponses=$copie->getReponses();
+        // on récupère la donnée qui a la clé "annotation-copie" dans la requête et on la pour dans annotation copie
+        $annotationCopie = $request->get('annotation-copie');
+        $copie->setAnnotation($annotationCopie);
+        // on récupère la donnée qui a la clé "annotation-note" dans la requête et on la pour dans annotation copie
+        $noteCopie = $request->get('note-copie');
+        $copie->setNote($noteCopie);
 
+        // on boucle sur la requête sous foorme de tableau, avec $key= "le nom du champ de formulaire" et value= "sa valeur"
+        foreach ($request->request->all() as $key=>$value){
+            // si la clé est différente de "annotation-copie" et "note-copie"
+            if($key !== "annotation-copie" && $key !== "note-copie"){
+
+                // on décompose la chaine de caractère de la clé avec le "-"  comme élément séparateur
+                $explode=explode( "-", $key );
+                //on cherche la 1ère réponse qui à l'ID = à $explode[1]
+                $reponse = $reponses->findFirst(function(int $index, Reponse $reponse) use($explode): bool {
+                    return $reponse->getId() == $explode[1];
+                });
+                // on vérifie que $explode[0] = à "annotation" avant d'envoyer  la valeur dans la table annotation
+                if ("annotation" == $explode[0]){
+                    $reponse->setAnnotation($value);
+                }
+                // on vérifie que $explode[0] = à "note" avant d'envoyer  la valeur dans la table annotation
+                elseif("note"== $explode[0]){
+                    $reponse->setNote($value);
+                }
+            }
+        }
+        $manager->flush();
+        return $this->redirectToRoute('app_copies_du_quiz', ['id'=>$copie->getQuiz()->getId()]);
     }
 }
